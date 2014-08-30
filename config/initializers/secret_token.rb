7346f01f1db1da70913f03d224484fba5d07d8ae
Luka Milekic <lmilekic@gmail.com>
2014-08-30 01:15:04 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HaikuGeneratorWebapp::Application.config.secret_token = 'cd8e34ab5860bfacd69970ec1313ed51a7495649c781bf09ff9cc66294709c28e1b31d5757b1b221c9e1a98648951bc6fa5fd2219e3890eabd9375f35d391cd1'
